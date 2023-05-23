5dcff20efa1b789bd7fe2195dbb0dc536aaca57d
MilanoBeer <tgd5753@gmail.com>
2023-05-24 00:59:49 +0900

    init : vite환경설정 / 폴더구조셋팅

---
+++ FE/seller-B-2/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>Vite</div>
+    </>
+  )
+}
+
+export default App
---
+++ FE/seller-B-2/src/Common/buttons/BaseButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BaseButton() {
+  return (
+    <div>BaseButton</div>
+  )
+}
+
+export default BaseButton
