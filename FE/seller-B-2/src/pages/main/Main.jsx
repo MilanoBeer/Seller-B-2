97821af99e5271222c77dcc23241d719f70d067c
MilanoBeer <tgd5753@gmail.com>
2023-05-31 23:55:58 +0900

    feat : useIsManagerHook에 따른 컴포넌트 분리

--- FE/seller-B-2/src/pages/main/Main.jsx
+++ FE/seller-B-2/src/pages/main/Main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import LoginContainer from '../../container/main/LoginContainer'
-
+import React from 'react';
+import ConsultantMain from '../consultant_main/ConsultantMain'
+import ManagerMain from '../manager_main/ManagerMain';
+import useIsManager from '../../hooks/main/useIsManager';
+import NavBar from '../../components/navigation/NavBar';
 
 function Main() {
-    return (
-        <>  
-        <LoginContainer />
-        </>
-    )
-  }
+  const { seq, isManager } = useIsManager();
 
+  return (
+    <>
+      <NavBar />
+      {isManager ? <ManagerMain /> : <ConsultantMain />}
+    </>
+  );
+}
 
-export default Main
+export default Main;
