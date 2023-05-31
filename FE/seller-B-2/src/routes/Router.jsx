ada524ae564d75766a7458565bbc0bc529a8951f
MilanoBeer <tgd5753@gmail.com>
2023-05-31 23:54:43 +0900

    feat : Entry, Main Route 추가

--- FE/seller-B-2/src/routes/Router.jsx
+++ FE/seller-B-2/src/routes/Router.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Main} from '../pages/index';
+import { Main, Entry} from '../pages/index';
 
 function Router() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Main />} />
+      <Route path='/' element={<Entry />} />
+      <Route path='/main' element={<Main />} />
+
 
     </Routes>
     </BrowserRouter>
