37dbe85ee02d8cdef4cea049b67a331de053e8b5
MilanoBeer <tgd5753@gmail.com>
2023-06-08 20:10:03 +0900

    feat : ConsultanMain.jsx

--- FE/seller-B-2/src/pages/consultant_main/ConsultantMain.jsx
+++ FE/seller-B-2/src/pages/consultant_main/ConsultantMain.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+import ConsultantNav from '../../components/common/navigation/ConsultantNav'
+
+// *** NEED COMPONENT ***
+// Consultant Nav
+// 출퇴근 Component 
+// 
 
 function ConsultantMain() {
   return (
-    <div>ConsultantMain</div>
+    <ConsultantMainContainer>
+      <div>ConsultantMain</div>
+    </ConsultantMainContainer>
+
   )
 }
+
+const ConsultantMainContainer = styled.div`
+  display: flex;
+  background-color: blue; 
+
+  width: 80vw;
+  height: 20vh;
+
+  margin: 0 auto;
+`
 
 export default ConsultantMain
