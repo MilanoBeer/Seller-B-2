aacc2ab6386e8f0922501ba5abe05785f1d060e3
MilanoBeer <tgd5753@gmail.com>
2023-06-09 03:04:58 +0900

    feat : login process - add useRef Hook - add validation - add Error check

--- FE/seller-B-2/src/components/main/LoginForm.jsx
+++ FE/seller-B-2/src/components/main/LoginForm.jsx
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
-
+import React, { useState, useRef, useEffect } from 'react';
+import validate from '../../hooks/entry/validate';
 import useLogin from '../../hooks/entry/useLogin';
 
+// validate test 
+const request = async (items) => {
+  const result = await new Promise( (resolve, reject) => {
+      setTimeout(() => {
+          resolve(items)
+      }, 1000)
+  }) 
+  alert(JSON.stringify(result))
+}
+
 function LoginForm() {
-    const { form, onChange, onSubmit } = useLogin();
-    const { email, password } = form;
+    const { email, password, onSubmit, errors, submit} = useLogin({email:'', password:''}, request, validate);
+    const inputRef = useRef();
+
+    useEffect(()=>{
+      inputRef.current.focus(); 
+    }, [])
 
   return (
     <div>
-    <h1 className="">Hello world!</h1>
-      <h1 className=''>로그인</h1>
-      <form onSubmit={onSubmit}>
+    <div className="px-6 py-5 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+    <div className="text-xl font-medium text-black">Login</div>
+
+    <form onSubmit={onSubmit}>
         <div>
-          <label>이메일</label>
-          <input type="text" value={email} onChange={onChange} name="email" />
+          <label htmlFor='email'>이메일</label>
+          <input ref={inputRef} type="text" name="email" {...email} />
+          {errors.email && <span> {errors.email}</span>}
         </div>
         <div>
-          <label>비밀번호</label>
-          <input
-            type="password"
-            value={password}
-            onChange={onChange}
-            name="password"
-          />
+          <label htmlFor='password'>비밀번호</label>
+          <input type="password" name="password" {...password} />
+          {errors.email && <span>{errors.password}</span>}
         </div>
-        <button type="submit">로그인</button>
-        <button className="" type="button" >
-                    count+
-                </button>
+        <button className="px-3 py-3" type="submit" value='로그인' disabled={submit}>로그인</button>
       </form>
+    </div>
     </div>
   )
 }
