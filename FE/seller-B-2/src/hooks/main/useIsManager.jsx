cfa01488073aeb8a4c12bc93ea010d5d9428317a
MilanoBeer <tgd5753@gmail.com>
2023-06-08 20:10:23 +0900

    fix : isManager state init value

--- FE/seller-B-2/src/hooks/main/useIsManager.jsx
+++ FE/seller-B-2/src/hooks/main/useIsManager.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 export default function useIsManager() {
 
     const [seq, setSeq] = useState("");
-    const [isManager, setIsManager] = useState(true);
+    const [isManager, setIsManager] = useState(false);
 
     // useEffect(() => {
     //     if (sessionStorage.getItem("adminCheck") === "ROLE_ADMIN") {
