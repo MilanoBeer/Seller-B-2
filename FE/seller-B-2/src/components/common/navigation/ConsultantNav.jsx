d47b60a1319fec084c42054856b51f212d0f92ef
MilanoBeer <tgd5753@gmail.com>
2023-06-08 20:27:20 +0900

    feat : add Container, NavItem Component - ConsultantNavContainer - ConsultantNavItem

--- FE/seller-B-2/src/components/common/navigation/ConsultantNav.jsx
+++ FE/seller-B-2/src/components/common/navigation/ConsultantNav.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 function ConsultantNav({seq, isManager}) {
     const navigate = useNavigate();
@@ -10,25 +11,48 @@ function ConsultantNav({seq, isManager}) {
       };
 
   return (
-    <div >
+    <ConsultantNavContainer>
+    <ConsultantNavItem>
     <div  onClick={() => navigate('/manager/noticeList')}>
       공지사항
     </div>
+    </ConsultantNavItem>
+
+    <ConsultantNavItem>
     <div onClick={() => navigate('/meeting/mancon')}>
       {/* <YoutubeOutlined id="meet-icon" /> */}
-    </div>
+    </div> 
+    </ConsultantNavItem>
+
+    <ConsultantNavItem>
     <div onClick={() => navigate('/meeting/consultingMain')}>
       상담
     </div>
+    </ConsultantNavItem>
+
+    <ConsultantNavItem>
     <div onClick={() => navigate(`/consultant/mypage/${seq}`)}>
       마이 페이지
     </div>
+    </ConsultantNavItem>
 
+    <ConsultantNavItem>
     <div onClick={onLogOutBtn}>
       로그아웃
     </div>
-  </div>
+    </ConsultantNavItem>
+    </ConsultantNavContainer>
   )
 }
+
+
+const ConsultantNavContainer = styled.div`
+  display: flex;
+`
+
+const ConsultantNavItem = styled.div`
+  margin: 2%;
+`
+
 
 export default ConsultantNav;
