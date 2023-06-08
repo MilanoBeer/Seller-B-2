f02145e004c71aa744c6f1f6241894f6f74af278
MilanoBeer <tgd5753@gmail.com>
2023-06-08 20:09:42 +0900

    feat : Entry.jsx

--- FE/seller-B-2/src/pages/entry/Entry.jsx
+++ FE/seller-B-2/src/pages/entry/Entry.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import LoginForm from '../../components/main/LoginForm'
+import styled from 'styled-components'
 
 function Entry() {
   return (
-    <>
-    <LoginForm />
-    </>
+    <EntryContainer>
+      <LoginForm />
+    </EntryContainer>
   )
 }
+
+const EntryContainer = styled.div`
+  background-color: grey;
+  width: 50vw;
+  height: 50vh;
+
+  padding: 10%;
+  margin: 0 auto;
+
+`
 
 export default Entry
